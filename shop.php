eadc93813ec8783198ee19c5b1cbadaed0bef65a
ChamiduDK <chamidudhilshan@gmail.com>
2025-03-11 02:43:31 +0530

    fix: correct stylesheet link in shop.php

--- shop.php
+++ shop.php
@@ -86,7 +86,7 @@ $connection->close();
     <title>Thambapanni Heritage - Shop</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/shop-styles.css.">
+    <link rel="stylesheet" href="css/shop-styles.css">
 </head>
 <body>
     <div class="container">
